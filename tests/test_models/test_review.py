982947a6175bbe0ba820af3394b61183ca1085fb
Airme1 <emmanuelochoja@yahoo.com>
2023-12-20 20:14:43 +0100

    test models

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,77 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unit tests for the Review class"""
+
+import unittest
+import os
 from models.review import Review
+from models.base_model import BaseModel
+import pep8
 
 
-class test_review(test_basemodel):
-    """ """
+class TestReview(unittest.TestCase):
+    """Test cases for the Review class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the tests"""
+        cls.rev = Review()
+        cls.rev.place_id = "4321-dcba"
+        cls.rev.user_id = "123-bca"
+        cls.rev.text = "The strongest in the Galaxy"
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up after the tests"""
+        del cls.rev
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    def tearDown(self):
+        """Tear down after each test"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+    def test_pep8_compliance(self):
+        """Check if the code complies with pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['models/review.py'])
+        self.assertEqual(result.total_errors, 0, "Fix pep8 issues")
+
+    def test_docstring_presence(self):
+        """Check if docstrings are present"""
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_attributes_existence(self):
+        """Check if Review class has required attributes"""
+        attributes = ['id', 'created_at', 'updated_at',
+                      'place_id', 'user_id', 'text']
+        for attribute in attributes:
+            self.assertTrue(attribute in self.rev.__dict__)
+
+    def test_is_subclass_of_base_model(self):
+        """Check if Review is a subclass of BaseModel"""
+        self.assertTrue(issubclass(self.rev.__class__, BaseModel))
+
+    def test_attribute_types(self):
+        """Check attribute types for Review class"""
+        self.assertEqual(type(self.rev.text), str)
+        self.assertEqual(type(self.rev.place_id), str)
+        self.assertEqual(type(self.rev.user_id), str)
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
+                     "Not applicable for db")
+    def test_save_method(self):
+        """Test if the save method works"""
+        self.rev.save()
+        self.assertNotEqual(self.rev.created_at, self.rev.updated_at)
+
+    def test_to_dict_method(self):
+        """Test if the to_dict method works"""
+        review_dict = self.rev.to_dict()
+        self.assertEqual(self.rev.__class__.__name__, 'Review')
+        self.assertIsInstance(review_dict['created_at'], str)
+        self.assertIsInstance(review_dict['updated_at'], str)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,72 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unit tests for the State class"""
+
+import unittest
+import os
 from models.state import State
+from models.base_model import BaseModel
+import pep8
 
 
-class test_state(test_basemodel):
-    """ """
+class TestState(unittest.TestCase):
+    """Test cases for the State class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the tests"""
+        cls.state = State()
+        cls.state.name = "CA"
 
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up after the tests"""
+        del cls.state
+
+    def tearDown(self):
+        """Tear down after each test"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_compliance(self):
+        """Check if the code complies with pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0, "Fix pep8 issues")
+
+    def test_docstring_presence(self):
+        """Check if docstrings are present"""
+        self.assertIsNotNone(State.__doc__)
+
+    def test_attributes_existence(self):
+        """Check if State class has required attributes"""
+        attributes = ['id', 'created_at', 'updated_at', 'name']
+        for attribute in attributes:
+            self.assertTrue(attribute in self.state.__dict__)
+
+    def test_is_subclass_of_base_model(self):
+        """Check if State is a subclass of BaseModel"""
+        self.assertTrue(issubclass(self.state.__class__, BaseModel))
+
+    def test_attribute_types(self):
+        """Check attribute type for State"""
+        self.assertEqual(type(self.state.name), str)
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
+                     "Not applicable for db")
+    def test_save_method(self):
+        """Test if the save method works"""
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_to_dict_method(self):
+        """Test if the to_dict method works"""
+        state_dict = self.state.to_dict()
+        self.assertEqual(self.state.__class__.__name__, 'State')
+        self.assertIsInstance(state_dict['created_at'], str)
+        self.assertIsInstance(state_dict['updated_at'], str)
+
+
+if __name__ == "__main__":
+    unittest.main()
